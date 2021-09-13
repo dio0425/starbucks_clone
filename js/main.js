9f1a5ae254c0f4fe47523593e1f0df4b7b5ad1bc
dio0425 <76507151+dio0425@users.noreply.github.com>
2021-09-14 01:29:42 +0900

    header css

--- js/main.js
+++ js/main.js
@@ -9,3 +9,8 @@ searchInputEl.addEventListener('focus', function () {
     searchEl.classList.add('focused');
     searchInputEl.setAttribute('placeholder', '통합검색');
 });
+
+searchInputEl.addEventListener('blur', function () {
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '');
+});
