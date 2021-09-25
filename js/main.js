fc6fe54bde7c7e329c85eb73cda36dfde46d9de9
dio0425 <76507151+dio0425@users.noreply.github.com>
2021-09-25 21:57:20 +0900

    swiper slide 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const searchEl = document.querySelector(".search");
 const searchInputEl = searchEl.querySelector("input");
 
+// search animation
 searchEl.addEventListener("click", function () {
   searchInputEl.focus();
 });
@@ -15,8 +16,10 @@ searchInputEl.addEventListener("blur", function () {
   searchInputEl.setAttribute("placeholder", "");
 });
 
+// badges function
 const badgeEl = document.querySelector("header .badges");
 
+// _.throttle(함수, 시간)
 window.addEventListener(
   "scroll",
   _.throttle(function () {
@@ -36,8 +39,8 @@ window.addEventListener(
     }
   }, 300)
 );
-// _.throttle(함수, 시간)
 
+// 순차적 요소 function
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
   // gsap.to(요소, 지속시간, 옵션);
@@ -45,4 +48,12 @@ fadeEls.forEach(function (fadeEl, index) {
     delay: (index - 1) * 0.7,
     opacity: 1,
   });
+});
+
+// new Swiper(선택자, 옵션)
+new Swiper(".notice-line .swiper-container", {
+  direction: "vertical", // 수직 정렬
+  slidesPerView: 1,
+  autoplay: true, // 자동 재생
+  loop: true, // 자동반복 재생
 });
