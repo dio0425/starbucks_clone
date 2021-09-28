578976eabb3b8310c56efdfde5a75dbeb92ef74b
dio0425 <76507151+dio0425@users.noreply.github.com>
2021-09-28 22:04:43 +0900

    Swiper를 이용한 슬라이드 추가

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ searchInputEl.addEventListener("blur", function () {
 
 // badges function
 const badgeEl = document.querySelector("header .badges");
-
 // _.throttle(함수, 시간)
 window.addEventListener(
   "scroll",
@@ -40,7 +39,7 @@ window.addEventListener(
   }, 300)
 );
 
-// 순차적 요소 function
+// 순차적 요소 보이기 function
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
   // gsap.to(요소, 지속시간, 옵션);
@@ -50,9 +49,29 @@ fadeEls.forEach(function (fadeEl, index) {
   });
 });
 
+// 공지사항 Swiper
 // new Swiper(선택자, 옵션)
 new Swiper(".notice-line .swiper-container", {
   direction: "vertical", // 수직 정렬
   autoplay: true, // 자동 재생
   loop: true, // 자동반복 재생
+});
+
+//promotion Swiper
+new Swiper(".promotion .swiper-container", {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
 });
